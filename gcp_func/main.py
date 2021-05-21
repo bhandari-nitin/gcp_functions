6ad37376ece7d028d20374baec6287dab0c60b7a
bhandari-nitin <nitinbhandari.cs@gmail.com>
2021-05-21 16:41:06 -0500

    Add files

--- gcp_func/main.py
+++ gcp_func/main.py
@@ -1,5 +1,22 @@
-from flask import Flask, request
-
-def main(request):
-	data = request.get_json()
-	return data
+def hello_world():
+    """Responds to any HTTP request.
+    Args:
+        request (flask.Request): HTTP request object.
+    Returns:
+        The response text or any set of values that can be turned into a
+        Response object using
+        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
+    """
+    
+    request_json = request.get_json()
+    
+    """
+    if request.args and 'message' in request.args:
+        return request.args.get('message')
+    elif request_json and 'message' in request_json:
+        return request_json['message']
+    else:
+        return f'Hello World!'
+    """
+    print(request_json)
+    return {"fulfillmentText": "response from webhook"}
